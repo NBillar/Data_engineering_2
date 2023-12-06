c4eb399c87f00050fedf0909ae99c6a8776e5e6a
WouterMulder9 <97562080+WouterMulder9@users.noreply.github.com>
2023-12-06 20:38:36 +0100

    Finalised the pipelines

--- kafka/kafka-admin.py
+++ kafka/kafka-admin.py
@@ -12,7 +12,7 @@ def create_topics(admin, topic_list):
 
 if __name__ == "__main__":
     admin_client = KafkaAdminClient(
-        bootstrap_servers="104.197.135.44:9092", client_id="Lab9"
+        bootstrap_servers="34.70.207.81:9092", client_id="Lab9"
     )  # use your VM's external IP Here!
     topic_list = [
         NewTopic(name="review", num_partitions=1, replication_factor=1),
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,7 +1,12 @@
+import subprocess
+import sys
+# subprocess.check_call([sys.executable, "-m", "pip", "uninstall", 'numpy'])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", 'numpy'])
 import json
 import pandas as pd
 import threading
 from kafka import KafkaProducer
+from tqdm import tqdm
 
 
 def kafka_python_producer_sync(producer, msg, topic):
@@ -28,6 +33,7 @@ def kafka_python_producer_async(producer, msg, topic):
 def produce_from_file(producer, file):
     data = pd.read_csv(file).drop(columns="Unnamed: 0")
     for index in data.index:
+
         kafka_python_producer_sync(
             producer, json.dumps(data.loc[index].to_dict()), "review"
         )
@@ -35,25 +41,26 @@ def produce_from_file(producer, file):
 
 def run_job():
     producer = KafkaProducer(
-        bootstrap_servers="104.154.219.138:9092"
+        bootstrap_servers="34.68.76.63:9092"
     )  # use your VM's external IP Here!
+    print('Sending Data!')
     # Change the path to your laptop!
     # if you want to learn about threading in python, check the following article
     # https://realpython.com/intro-to-python-threading/
     # if you want to schedule a job https://www.geeksforgeeks.org/python-schedule-library/
-    t1 = threading.Thread(target=produce_from_file, args=(producer, "data\Review1.csv"))
-    # t2 = threading.Thread(target=produce_from_file, args=(producer, "data\Review2.csv"))
-    # t3 = threading.Thread(target=produce_from_file, args=(producer, "data\Review3.csv"))
-    # t4 = threading.Thread(target=produce_from_file, args=(producer, "data\Review4.csv"))
+    t1 = threading.Thread(target=produce_from_file, args=(producer, "C:/Users/woute/OneDrive/Documenten/GitHub/Wouter/Data_engineering_2/data/Review1.csv"))
+    t2 = threading.Thread(target=produce_from_file, args=(producer, "C:/Users/woute/OneDrive/Documenten/GitHub/Wouter/Data_engineering_2/data/Review2.csv"))
+    t3 = threading.Thread(target=produce_from_file, args=(producer, "C:/Users/woute/OneDrive/Documenten/GitHub/Wouter/Data_engineering_2/data/Review3.csv"))
+    t4 = threading.Thread(target=produce_from_file, args=(producer, "C:/Users/woute/OneDrive/Documenten/GitHub/Wouter/Data_engineering_2/data/Review4.csv"))
     t1.start()
-    # t2.start()
-    # t3.start()
-    # t4.start()
+    t2.start()
+    t3.start()
+    t4.start()
 
     t1.join()
-    # t2.join()
-    # t3.join()
-    # t4.join()
+    t2.join()
+    t3.join()
+    t4.join()
 
 
 if __name__ == "__main__":
